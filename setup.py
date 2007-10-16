5539c4090a315d2a8d74293c4b3cf9686592a604
Luke Tucker <ltucker@openplans.org>
2007-10-16 20:40:51 +0000

    adding melk.util package
    git-svn-id: https://svn.openplans.org/melk/melk.util/trunk@56 0334193d-493b-0410-8260-970145f4dd2d

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='melk.util',
+    version="0.1",
+    description="General utilities used by melkjug",
+    author="Luke Tucker",
+    author_email="ltucker@openplans.org",
+    #url="",
+    install_requires=[
+    ],
+    dependency_links=[
+    ],
+    packages=find_packages(),
+    namespace_packages=['melk'],
+    include_package_data=True,
+    test_suite = 'nose.collector',
+    entry_points="""
+    """,
+)
