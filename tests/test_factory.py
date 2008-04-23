ef611517c48c65e85730378fc4ba4472df94b30b
Luke Tucker <ltucker@openplans.org>
2008-04-23 15:39:22 +0000

    add case for no constructor args to factory tests
    git-svn-id: https://svn.openplans.org/melk/melk.util/trunk@651 0334193d-493b-0410-8260-970145f4dd2d

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -4,6 +4,7 @@ from melk.util.factory import BasicObjectURIFactory
 def test_object_uri_factory_basic():
     foo_uri = 'http://www.example.org/foo?quux=baz'
     bar_uri = 'http://www.example.org/bar?quux=bazoo'
+    baz_uri = 'http://www.example.org/baz'
     
     class Foo:
         def __init__(self, quux):
@@ -13,10 +14,14 @@ def test_object_uri_factory_basic():
         def __init__(self, quux):
             self.quux = quux
     
+    class Baz:
+        def __init__(self):
+            pass
+            
     factory = BasicObjectURIFactory()
     factory.register(foo_uri, Foo)
     factory.register(bar_uri, Bar)
-    
+    factory.register(baz_uri, Baz)
     
     foo_ob = factory.create_from_uri(foo_uri) 
     assert(foo_ob.__class__ == Foo)
@@ -26,5 +31,5 @@ def test_object_uri_factory_basic():
     assert(bar_ob.__class__ == Bar)
     assert(bar_ob.quux == u'bazoo')
     
-    
-    
+    baz_ob = factory.create_from_uri(baz_uri) 
+    assert(baz_ob.__class__ == Baz)
